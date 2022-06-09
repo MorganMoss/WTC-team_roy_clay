6c5e3bebd971aca774aa163882469647b86aa690
mmoss <mmoss020@student.wethinkcode.co.za>
2022-06-09 12:27:38 +0200

    minor bugfix

--- run_with_maven.sh
+++ run_with_maven.sh
@@ -1,2 +1,2 @@
-mvn compile exec:java -Dexec.mainClass="za.co.wethinkcode.robotworlds.$1" -Dexec.args="$2" > "Test Output/out - $1.txt" 2> "Test Output/err - $1.txt" &
+mvn compile exec:java -Dexec.mainClass="za.co.wethinkcode.robotworlds.$1" -Dexec.args="$2" > "Test Output/out - $1 $2.txt" 2> "Test Output/err - $1 $2.txt" &
 echo $! > run.pid
