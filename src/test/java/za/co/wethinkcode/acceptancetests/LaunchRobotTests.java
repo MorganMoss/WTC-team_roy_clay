79e3a80fdcdc771ff75e1a7bbb107155c011027f
pebongue021 <pebongue021@student.wethinkcode.co.za>
2022-06-09 16:15:21 +0200

    added and tested scenario for when the world has not more space for a robot to launch. All test passing on reference server

--- src/test/java/za/co/wethinkcode/acceptancetests/LaunchRobotTests.java
+++ src/test/java/za/co/wethinkcode/acceptancetests/LaunchRobotTests.java
@@ -31,9 +31,29 @@ public class LaunchRobotTests {
     void disconnectFromServer(){
         serverClient.disconnect();
     }
+
+    /**
+     * Below are functions to assert arguments and avoid repeating lines of code in tests
+     */
+    private void assertResult(JsonNode response, String status){
+        assertNotNull(response.get("result"));
+        assertEquals(status, response.get("result").asText());
+    }
+
+    private void assertMessage(JsonNode response, String message){
+        assertNotNull(response.get("data"));
+        assertNotNull(response.get("data").get("message"));
+        assertTrue(response.get("data").get("message").asText().contains(message));
+    }
+
+    private void assertPosition(JsonNode response, int x, int y){
+        assertNotNull(response.get("data"));
+        assertNotNull(response.get("data").get("position"));
+        assertEquals(x, response.get("data").get("position").get(0).asInt());
+        assertEquals(y, response.get("data").get("position").get(1).asInt());
+    }
+
     @Test
-
-
     void validLaunchShouldSucceed(){
         // Given that I am connected to a running Robot Worlds server
         // And the world is of size 1x1 (The world is configured or hardcoded to this size)
@@ -49,14 +69,10 @@ public class LaunchRobotTests {
         JsonNode response = serverClient.sendRequest(request);
 
         // Then I should get a valid response from the server
-        assertNotNull(response.get("result"));
-        assertEquals("OK", response.get("result").asText());
+        assertResult(response, "OK");
 
         // And the position should be (x:0, y:0)
-        assertNotNull(response.get("data"));
-        assertNotNull(response.get("data").get("position"));
-        assertEquals(0, response.get("data").get("position").get(0).asInt());
-        assertEquals(0, response.get("data").get("position").get(1).asInt());
+        assertPosition(response, 0, 0);
 
         // And I should also get the state of the robot
         assertNotNull(response.get("state"));
@@ -66,7 +82,7 @@ public class LaunchRobotTests {
         // Given that I am connected to a running Robot Worlds server
         assertTrue(serverClient.isConnected());
 
-        // When I send a invalid launch request with the command "luanch" instead of "launch"
+        // When I send an invalid launch request with the command "luanch" instead of "launch"
         String request = "{" +
                 "\"robot\": \"HAL\"," +
                 "\"command\": \"luanch\"," +
@@ -76,21 +92,16 @@ public class LaunchRobotTests {
         JsonNode response = serverClient.sendRequest(request);
 
         // Then I should get an error response
-        assertNotNull(response.get("result"));
-        assertEquals("ERROR", response.get("result").asText());
+        assertResult(response, "ERROR");
 
         // And the message "Unsupported command"
-        assertNotNull(response.get("data"));
-        assertNotNull(response.get("data").get("message"));
-        assertTrue(response.get("data").get("message").asText().contains("Unsupported command"));
+        assertMessage(response, "Unsupported command");
     }
 
     @Test
     void checkForDuplicateRobotName(){
         // Given that I am connected to a running Robot Worlds server
         assertTrue(serverClient.isConnected());
-
-        //set the world size to 5x5
 
         // When I send a valid launch request to the server
         String request = "{" +
@@ -102,8 +113,7 @@ public class LaunchRobotTests {
         JsonNode response = serverClient.sendRequest(request);
 
         // Then I should get a valid response from the server
-        assertNotNull(response.get("result"));
-        assertEquals("OK", response.get("result").asText());
+        assertResult(response, "OK");
 
         // And I should also get the state of the robot
         assertNotNull(response.get("state"));
@@ -118,12 +128,48 @@ public class LaunchRobotTests {
         JsonNode response_2 = serverClient.sendRequest(request_2);
 
         // Then I should get an invalid or error response from the server
-        assertNotNull(response_2.get("result"));
-        assertEquals("ERROR", response_2.get("result").asText());
+        assertResult(response_2, "ERROR");
 
         // And the message "Too many of you in this world"
-        assertNotNull(response_2.get("data"));
-        assertNotNull(response_2.get("data").get("message"));
-        assertTrue(response_2.get("data").get("message").asText().contains("Too many of you in this world"));
+        assertMessage(response_2, "Too many of you in this world");
+    }
+
+    @Test
+    void worldFullNoSpaceToLaunchRobot(){
+        // Given that I am connected to a running Robot Worlds server
+        assertTrue(serverClient.isConnected());
+
+        // When I send a valid launch request to the server
+        String request = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+
+        JsonNode response = serverClient.sendRequest(request);
+
+        // Then I should get a valid response from the server
+        assertResult(response, "OK");
+
+        // And the position should be (x:0, y:0)
+        assertPosition(response, 0, 0);
+
+        // And I should also get the state for the launched robot
+        assertNotNull(response.get("state"));
+
+        // When I send another valid launch request for a different robot named "TRAVIS" to the server
+        String request_2 = "{" +
+                "  \"robot\": \"TRAVIS\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"7\",\"4\"]" +
+                "}";
+
+        JsonNode response_2 = serverClient.sendRequest(request_2);
+
+        // Then I should get an invalid or error response from the server
+        assertResult(response_2, "ERROR");
+
+        // And the message "No more space in this world"
+        assertMessage(response_2, "No more space in this world");
     }
 }
