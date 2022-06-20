8d4b2418725253425ef6c553ca2ecf4ea49a2394
mmoss <mmoss020@student.wethinkcode.co.za>
2022-06-20 16:25:17 +0200

    added more skeleton code for entities.

--- Server/src/main/java/za/co/wethinkcode/server/handler/world/World.java
+++ Server/src/main/java/za/co/wethinkcode/server/handler/world/World.java
@@ -4,6 +4,7 @@ import za.co.wethinkcode.server.handler.world.map.Map;
 
 public class World extends AbstractWorld{
 
+    //TODO: Move blocks position and path responsibility to the world.
     public World(Map maze) {
         super(maze);
     }
--- Server/src/main/java/za/co/wethinkcode/server/handler/world/entity/immovable/Pit.java
+++ Server/src/main/java/za/co/wethinkcode/server/handler/world/entity/immovable/Pit.java
@@ -1,4 +1,29 @@
 package za.co.wethinkcode.server.handler.world.entity.immovable;
 
-public class Pit {
+import za.co.wethinkcode.server.handler.world.entity.movable.Movable;
+
+import java.awt.*;
+
+/**
+ * A pit is a stationary entity a robot can fall into
+ */
+public class Pit extends Immovable {
+    /**
+     * An immovable entity is instantiated with its final position
+     * @param position of this entity
+     */
+    protected Pit(Point position) {
+        super(position);
+    }
+
+    /**
+     * Walking into a pit will instantly kill a robot
+     * @param entity The entity moving into the same space as this entity.
+     * @return "Fell"
+     */
+    @Override
+    public String collidedWith(Movable entity) {
+        //Kill the movable entity
+        return "Fell";
+    }
 }
