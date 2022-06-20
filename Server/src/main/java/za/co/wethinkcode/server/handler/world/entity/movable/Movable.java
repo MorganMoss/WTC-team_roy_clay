9af509e37c350f7f816c8032398a0921dce951bc
mmoss <mmoss020@student.wethinkcode.co.za>
2022-06-20 10:09:11 +0200

    added some method skeleton code

--- Server/src/main/java/za/co/wethinkcode/server/handler/world/entity/movable/Movable.java
+++ Server/src/main/java/za/co/wethinkcode/server/handler/world/entity/movable/Movable.java
@@ -2,5 +2,16 @@ package za.co.wethinkcode.server.handler.world.entity.movable;
 
 import za.co.wethinkcode.server.handler.world.entity.Entity;
 
-public interface Movable extends Entity {
+import java.awt.*;
+
+public abstract class Movable implements Entity {
+    protected Point position;
+
+    /**
+     * Change the position of the entity
+     * @param newPosition the new position of this entity
+     */
+    void updatePosition(Point newPosition){
+        position = newPosition;
+    }
 }
