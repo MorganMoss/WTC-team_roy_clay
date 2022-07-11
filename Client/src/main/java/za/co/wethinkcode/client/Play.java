d23f141df860ae55b106a37562becd9843cc4c7f
mmoss <mmoss020@student.wethinkcode.co.za>
2022-07-11 16:22:18 +0200

    made Play usable

--- Client/src/main/java/za/co/wethinkcode/client/Play.java
+++ Client/src/main/java/za/co/wethinkcode/client/Play.java
@@ -1,138 +1,75 @@
 package za.co.wethinkcode.client;
 
-import org.json.JSONObject;
-import za.co.wethinkcode.robotworlds.maze.*;
-import za.co.wethinkcode.robotworlds.world.*;
-import za.co.wethinkcode.robotworlds.world.AbstractWorld;
+import za.co.wethinkcode.Request;
+import za.co.wethinkcode.Response;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TODO: leave the client alone until we need to use it.
 public class Play {
-    static Scanner scanner;
-    
+    static Scanner scanner = new Scanner(System.in);;
 
     public static void main(String[] args) throws Exception {
-        int count = 0;
-        scanner = new Scanner(System.in);
         //create socket
-        Socket s=new Socket("localhost",3333);
+        Socket s = new Socket("localhost", 5000);
 
-        DataInputStream din=new DataInputStream(s.getInputStream());
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
-        //set robot
-        String name = getInput("What do you want to name your robot?");
-        System.out.println("Hello Kiddo!");
-        AbstractWorld world = worldSelector(args);
-        Robot robot= new Robot(name,world);
+        String in;
+        String name;
 
-        //creating and sending json with starting info for robot
-
-        JSONObject json = new JSONObject();
-        json.put("robot",robot);
-        json.put("name",name);
-        json.put("world",world);
-        while (count ==0){
-            count++;
-            dout.writeUTF(json.toString());
-            dout.flush();
-        }
-
-
-
-
-        boolean shouldContinue = true;
         do {
-            String instruction = getInput(robot.getName() + "> What must I do next?").strip().toLowerCase();
-            json.put("command",instruction);
-            try {
+             name = getInput("What do you want to name your robot?");
 
-                Command command = Command.create(instruction);
-                shouldContinue=robot.handleCommand(command);
+            dout.writeUTF(new Request(name, "Launch", new ArrayList<>()).serialize());
+            dout.flush();
+            in = din.readUTF();
+            System.out.println();
+        } while (Response.deSerialize(in).getResult().equals("OK"));
 
-
-
-                dout.writeUTF(json.toString());
-                dout.flush();
-
-                String in;
-
-                shouldContinue = din.readBoolean();
-                System.out.println("doing thing");
-//                System.out.println(shouldContinue);
+        do {
+            String[] instruction = getInput(name + "> What must I do next?").strip().toLowerCase().split(" ", 1);
 
 
+            String command = instruction[0];
+            String arguments = instruction[1];
 
-            } catch (IllegalArgumentException e) {
-                robot.setStatus("Sorry, I did not understand '" + instruction + "'.");
+            if (command.equals("quit")) {
+                break;
             }
-            System.out.println(robot);
-        } while (shouldContinue);
+
+            Request request = new Request(name, command, List.of(arguments.split(" ")));
+
+            dout.writeUTF(request.serialize());
+            dout.flush();
+
+
+
+            in = din.readUTF();
+
+            System.out.println(in);
+        } while (true);
+
         dout.close();
         s.close();
-
     }
 
-
-
-
     public static String getInput(String prompt) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println(prompt);
+
         String input = scanner.nextLine();
+
         while (input.isBlank()) {
             System.out.println(prompt);
             input = scanner.nextLine();
         }
+
         return input;
-    }
-
-    public static AbstractWorld worldSelector(String []args){
-        AbstractWorld world = null;
-
-        if (args.length == 0 || args[0].equalsIgnoreCase("text"))
-        {
-            if (args.length == 0 || args.length==1 && args[0].equalsIgnoreCase("text") || args[1].equalsIgnoreCase("emptymaze") )
-            {
-                System.out.println("Loaded EmptyMaze.");
-                 world = new TextWorld(new EmptyMaze());
-
-            }
-            else if(args[1].equalsIgnoreCase("simplemaze"))
-            {
-                System.out.println("Loaded SimpleMaze.");
-                world = new TextWorld(new SimpleMaze());
-
-            }
-            else if(args[1].equalsIgnoreCase("randommaze"))
-            {
-                System.out.println("Loaded RandomMaze.");
-                 world = new TextWorld(new RandomMaze());
-            }}else if (args[0].equalsIgnoreCase("turtle"))
-        {
-            if ( args.length==1 && args[0].equalsIgnoreCase("turtle") ||args[1].equalsIgnoreCase("randommaze"))
-            {
-                System.out.println("Loaded RandomMaze.");
-                 world = new TurtleWorld(new RandomMaze());
-
-            }
-            else if(args[1].equalsIgnoreCase("simplemaze"))
-            {
-                System.out.println("Loaded SimpleMaze.");
-                 world = new TurtleWorld(new SimpleMaze());
-
-            }
-            else if(args[1].equalsIgnoreCase("emptymaze"))
-            {
-                System.out.println("Loaded EmptyMaze.");
-                 world = new TurtleWorld(new EmptyMaze());
-
-            }
-        }
-        return world;
     }
 }
